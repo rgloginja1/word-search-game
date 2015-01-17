4feb05bc1c5de9a25e6656145815664f06e0f5c7
Robert Gloginja <robertgloginja@gmail.com>
2015-01-17 12:11:26 +1100

    Update config.php
    More variables for user customization added.
    Line 10 (when uncommented) will redirect to dberror.php if we get a DB error.

--- config.php
+++ config.php
@@ -5,18 +5,24 @@
 	define('DB_PASS', '');
 	define('DB_NAME', '');
 
-	$link = mysql_connect(DB_HOST, DB_USER, DB_PASS) or die('Could not connect to MySQL database ') . mysql_error();
+	# Please delete the comment on the next line when the database
+	# configuration on the above lines are set. 
+	$link = mysql_connect(DB_HOST, DB_USER, DB_PASS); # or die ('<META HTTP-EQUIV="Refresh" CONTENT="0;URL=dberror.php">');
 	$db = mysql_select_db(DB_NAME, $link);
 
 	# App Configuration
-	$app_name = "Word Finder";
-	$app_slogan = "Words. Words. Words.";
+	$app_name = "Word Hunter";
+	$app_slogan = "A project by RMG Software Pty Ltd";
 	
 	# Here you can provide a name from the following list: cerulean,
 	# cosmo, cyborg, darkly, flatly, journal, lumen, paper, readable,
 	# sandstone, simplex, slate, spacelab, superhero, united, yeti
-	# 
+
 	# For more information, please visit http://www.bootstrapcdn.com/#bootswatch_tab
 	$app_theme = "cosmo";
 	
+	# Error redirection - These variable will be required for all error pages
+	$take_me_home = "http://www.google.com.au";
+	$contact_support = "http://www.google.com.au";
+
 ?>
